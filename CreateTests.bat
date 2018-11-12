62c92abc651be68c4ecf28848342ae42c182d2c5
Cedd Burge <cedd.burge@res-group.com>
2018-11-12 08:34:08 +0000

    Add script to create / recreate the c# tests from excel

---
+++ CreateTests.bat
@@ -0,0 +1,1 @@
+packages\CustomerTestsExcel.1.0.2\tools\GenerateCodeFromExcelTest.exe /folder "CustomerTests" /project CustomerTests.csproj /namespace CustomerTests /usings "CustomerTests.IgnoreOnGeneration" /assertionClassPrefix "I"
